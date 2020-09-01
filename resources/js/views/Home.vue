fbe443affa7a71321eda8ff99b477b8580f9c307
Dendy Armandiaz <dendy.aziz@gmail.com>
2020-09-01 23:06:41 +0700

    skills layout

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -5,10 +5,13 @@
             <div class="columns reverse-order">
                 <div class="column">
                     <h1 class="has-text-weight-semibold mb-2">I am <span class="point">Dendy Armandiaz</span></h1>
-                    <h4 class="mb-4">I am a Front-end Developer living in Bekasi, Indonesia. I recently work as a freelance Remote Web Developer for Telkom University, located in Bandung. I am looking to collaborate with you as a Front-end Developer.</h4>
+                    <h4 class="mb-4">I am a Front-end Developer living in Bekasi, Indonesia. I recently work as a
+                        freelance Remote Web Developer for Telkom University, located in Bandung. I am looking to
+                        collaborate with you as a Front-end Developer.</h4>
                     <div class="columns mb-0 is-mobile">
                         <div class="column is-3-desktop is-4-mobile info-title">Whatsapp</div>
-                        <div class="column"><span class="mr-1">+62</span><span class="mr-1">896</span><span class="mr-1">0943</span><span>3526</span></div>
+                        <div class="column"><span class="mr-1">+62</span><span class="mr-1">896</span><span
+                                class="mr-1">0943</span><span>3526</span></div>
                     </div>
                     <div class="columns mb-0 is-mobile">
                         <div class="column is-3-desktop is-4-mobile info-title">Email</div>
@@ -20,10 +23,18 @@
                     </div>
                     <div class="columns is-mobile">
                         <div class="column is-3-desktop is-4-mobile info-title">Social</div>
-                        <div class="column is-narrow"><a href="https://linkedin.com/in/dendyaziz" target="_blank"><b-icon pack="fab" icon="linkedin-in" size="is-small"></b-icon></a></div>
-                        <div class="column is-narrow"><a href="https://instagram.com/dendyaziz" target="_blank"><b-icon pack="fab" icon="instagram" size="is-small"></b-icon></a></div>
-                        <div class="column is-narrow"><a href="https://github.com/dendyaziz" target="_blank"><b-icon pack="fab" icon="github" size="is-small"></b-icon></a></div>
-                        <div class="column is-narrow"><a href="https://dribbble.com/dendyaziz" target="_blank"><b-icon pack="fab" icon="dribbble" size="is-small"></b-icon></a></div>
+                        <div class="column is-narrow"><a href="https://linkedin.com/in/dendyaziz" target="_blank">
+                            <b-icon pack="fab" icon="linkedin-in" size="is-small"></b-icon>
+                        </a></div>
+                        <div class="column is-narrow"><a href="https://instagram.com/dendyaziz" target="_blank">
+                            <b-icon pack="fab" icon="instagram" size="is-small"></b-icon>
+                        </a></div>
+                        <div class="column is-narrow"><a href="https://github.com/dendyaziz" target="_blank">
+                            <b-icon pack="fab" icon="github" size="is-small"></b-icon>
+                        </a></div>
+                        <div class="column is-narrow"><a href="https://dribbble.com/dendyaziz" target="_blank">
+                            <b-icon pack="fab" icon="dribbble" size="is-small"></b-icon>
+                        </a></div>
                     </div>
                     <div class="columns mt-3">
                         <div class="column is-narrow py-1">
@@ -35,107 +46,48 @@
                     </div>
                 </div>
                 <div class="column has-text-centered">
-                    <img src="https://i.ibb.co/N9Hxzdn/Profile-Picture.png" class="profile-picture image-fit" alt="Profile Picture">
+                    <img src="https://i.ibb.co/N9Hxzdn/Profile-Picture.png" class="profile-picture image-fit"
+                         alt="Profile Picture">
                 </div>
             </div>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+        </div>
+        <hr class="separator">
+        <div class="container">
+            <div class="columns is- py-2">
+                <div class="column is-one-thirds">
+                    <h1 class="has-text-weight-semibold mb-2"><span class="point">Skills</span></h1>
+                    <h4 class="mb-4">I am inspired by creating great work with people who are as passionate as I am
+                        about building something awesome.</h4>
+
+                </div>
+                <div class="column is-two-thirds">
+                    <div class="columns is-multiline">
+                        <div class="column is-half py-0">
+                            <h3 class="has-text-weight-semibold mb-2">Skill 1</h3>
+                            <h4>I am inspired by creating great work with people who are as passionate as I
+                                am</h4>
+                        </div>
+                        <div class="column is-half py-0">
+                            <hr class="is-hidden-tablet">
+                            <h3 class="has-text-weight-semibold mb-2">Skill 2</h3>
+                            <h4>I am inspired by creating great work with people who are as passionate as I
+                                am about building something awesome.</h4>
+                        </div>
+                        <div class="column is-half py-0">
+                            <hr>
+                            <h3 class="has-text-weight-semibold mb-2">Skill 3</h3>
+                            <h4>I am inspired by creating great work with people who are as passionate as I
+                                am about building something awesome.</h4>
+                        </div>
+                        <div class="column is-half py-0">
+                            <hr>
+                            <h3 class="has-text-weight-semibold mb-2">Skill 4</h3>
+                            <h4>I am inspired by creating great work with people who are as passionate as I
+                                am</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
