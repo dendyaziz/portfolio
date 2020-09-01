9c28298af86dcd7dd385f176bdb0f65b5bd0d3bd
Dendy Armandiaz <dendy.aziz@gmail.com>
2020-09-01 22:12:41 +0700

    Top section done

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -2,8 +2,41 @@
     <section>
         <Navbar/>
         <div class="container">
-            <h1 class="has-text-weight-semibold mb-2">I am <span class="point">Dendy Armandiaz</span></h1>
-            <h4>I am a Front-end Developer located in Bekasi, Indonesia. I recently work as a freelance Remote Web Developer for Telkom University, located in Bandung. I am looking to collaborate with you as a Front-end Developer.</h4>
+            <div class="columns reverse-order">
+                <div class="column">
+                    <h1 class="has-text-weight-semibold mb-2">I am <span class="point">Dendy Armandiaz</span></h1>
+                    <h4 class="mb-4">I am a Front-end Developer living in Bekasi, Indonesia. I recently work as a freelance Remote Web Developer for Telkom University, located in Bandung. I am looking to collaborate with you as a Front-end Developer.</h4>
+                    <div class="columns mb-0 is-mobile">
+                        <div class="column is-3-desktop is-4-mobile info-title">Whatsapp</div>
+                        <div class="column"><span class="mr-1">+62</span><span class="mr-1">896</span><span class="mr-1">0943</span><span>3526</span></div>
+                    </div>
+                    <div class="columns mb-0 is-mobile">
+                        <div class="column is-3-desktop is-4-mobile info-title">Email</div>
+                        <div class="column">dendy.aziz@gmail.com</div>
+                    </div>
+                    <div class="columns mb-0 is-mobile">
+                        <div class="column is-3-desktop is-4-mobile info-title">Address</div>
+                        <div class="column">East Cikarang, Bekasi, Indonesia 17825</div>
+                    </div>
+                    <div class="columns is-mobile">
+                        <div class="column is-3-desktop is-4-mobile info-title">Social</div>
+                        <div class="column is-narrow"><a href="https://linkedin.com/in/dendyaziz" target="_blank"><b-icon pack="fab" icon="linkedin-in" size="is-small"></b-icon></a></div>
+                        <div class="column is-narrow"><a href="https://instagram.com/dendyaziz" target="_blank"><b-icon pack="fab" icon="instagram" size="is-small"></b-icon></a></div>
+                        <div class="column is-narrow"><a href="https://dribbble.com/dendyaziz" target="_blank"><b-icon pack="fab" icon="dribbble" size="is-small"></b-icon></a></div>
+                    </div>
+                    <div class="columns mt-3">
+                        <div class="column is-narrow py-1">
+                            <b-button type="is-primary" class="is-boxy" expanded>Contact Me</b-button>
+                        </div>
+                        <div class="column is-narrow py-1">
+                            <b-button type="is-light" class="is-boxy" expanded>Download CV</b-button>
+                        </div>
+                    </div>
+                </div>
+                <div class="column has-text-centered">
+                    <img src="https://i.ibb.co/TM1SMVZ/Profile-Picture.png" class="profile-picture image-fit" alt="Profile Picture">
+                </div>
+            </div>
             <br>
             <br>
             <br>
@@ -108,9 +141,10 @@
 <script>
 
     import Navbar from "../layouts/Navbar";
+    import BIcon from "buefy/src/components/icon/Icon";
 
     export default {
-        components: {Navbar},
+        components: {BIcon, Navbar},
         data() {
             return {}
         }
